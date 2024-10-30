b799f03e2ba62fa4d7e4407873eb4c8a16a0e322
Sarvesh-T16 <sarveshthaleshwar@gmail.com>
2024-10-30 14:27:32 +0000

    Added files

---
+++ BankAccount2/src/test/java/TestBankAccount.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestBankAccount {
+
+    @Test
+    public void testDeposit(){
+        BankAccount bankAccount = new BankAccount();
+        double value1 = bankAccount.deposit(200);
+        assertEquals(,);
+
+    }
+}
